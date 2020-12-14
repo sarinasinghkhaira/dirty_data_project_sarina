aa3dd985a9140c0d7c02add99197dc819f2cc786
Sarina Singh Khaira <sarina.k92@gmail.com>
2020-12-14 18:15:17 +0000

    combined 3 tables with bind_rows and selected relevant cols

--- task_4/data_cleaning_scripts/candy_data_cleaning.R
+++ task_4/data_cleaning_scripts/candy_data_cleaning.R
@@ -19,18 +19,32 @@ names(candy_2017) <- c17_names_removed
 
 #Rename columns so they match
 candy_2015 <- candy_2015 %>%
-  rename(timestamp_id = timestamp,
-         age = how_old_are_you,
+  rename(age = how_old_are_you,
          going_out = are_you_going_actually_going_trick_or_treating_yourself)
 
 
 candy_2016 <- candy_2016 %>%
-  rename(timestamp_id = timestamp,
-         age = how_old_are_you,
+  rename(age = how_old_are_you,
          going_out = are_you_going_actually_going_trick_or_treating_yourself,
          country = which_country_do_you_live_in,
-         state = which_state_province_county_do_you_live_in)
+         state = which_state_province_county_do_you_live_in,
+         gender = your_gender)
 
 candy_2017 <- candy_2017 %>%
-  rename(timestamp_id = internal_id,
-         state = state_province_county_etc)
+  rename(state = state_province_county_etc)
+
+#Combine 3 tables 
+candy <- bind_rows(candy_2015, candy_2016, candy_2017)
+
+#Select columms with 4 distinct values, and other relevant cols
+candy_clean <- candy %>% select(
+  internal_id,
+  age,
+  going_out,
+  timestamp,
+  country,
+  state,
+  gender,
+  where(~n_distinct(.) == 4))
+
+#Notes: in
