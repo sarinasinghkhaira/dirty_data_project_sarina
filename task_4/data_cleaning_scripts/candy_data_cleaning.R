a22fe7b2dfee6acbba278420aeded35175ac51b7
Sarina Singh Khaira <sarina.k92@gmail.com>
2020-12-14 16:55:15 +0000

    renamed columns to match each year table

--- task_4/data_cleaning_scripts/candy_data_cleaning.R
+++ task_4/data_cleaning_scripts/candy_data_cleaning.R
@@ -17,5 +17,20 @@ candy_2017 <- read_excel(here("raw_data/boing-boing-candy-2017.xlsx")) %>%
 c17_names_removed<- str_remove_all(names(candy_2017), "q[0-9]+_")
 names(candy_2017) <- c17_names_removed
 
-#
+#Rename columns so they match
+candy_2015 <- candy_2015 %>%
+  rename(timestamp_id = timestamp,
+         age = how_old_are_you,
+         going_out = are_you_going_actually_going_trick_or_treating_yourself)
 
+
+candy_2016 <- candy_2016 %>%
+  rename(timestamp_id = timestamp,
+         age = how_old_are_you,
+         going_out = are_you_going_actually_going_trick_or_treating_yourself,
+         country = which_country_do_you_live_in,
+         state = which_state_province_county_do_you_live_in)
+
+candy_2017 <- candy_2017 %>%
+  rename(timestamp_id = internal_id,
+         state = state_province_county_etc)
